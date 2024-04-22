19c38529b21261d2a0269862802d37867525e689
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-04-22 23:56:54 +0300

    [Feature] Web Application Security - oauth2

--- catalogue-service/src/main/java/com/javaded/config/SecurityBeans.java
+++ catalogue-service/src/main/java/com/javaded/config/SecurityBeans.java
@@ -2,8 +2,10 @@ package com.javaded.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,14 +13,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityBeans {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers("/catalogue-api/**")
-                                .hasRole("SERVICE"))
-                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/products")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue-api/products/{productId:\\d}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_catalogue")
+                        .anyRequest().denyAll())
+                .csrf(CsrfConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)                )
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
                 .build();
     }
 }
--- manager-app/src/main/java/com/javaded/config/ClientBeans.java
+++ manager-app/src/main/java/com/javaded/config/ClientBeans.java
@@ -1,24 +1,31 @@
 package com.javaded.config;
 
+
 import com.javaded.client.RestClienProductsRestClient;
+import com.javaded.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientBeans {
 
     @Bean
-    public RestClienProductsRestClient productsRestClient(
+    public RestClienProductsRestClient restClienProductsRestClient(
             @Value("${catalogue.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
-            @Value("${catalogue.services.catalogue.username:}") String username,
-            @Value("${catalogue.services.catalogue.password:}") String password) {
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${catalogue.services.catalogue.registration-id:keycloak}") String registrationId) {
         return new RestClienProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
                 .requestInterceptor(
-                        new BasicAuthenticationInterceptor(username, password))
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
                 .build());
     }
 }
