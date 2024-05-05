48096684a5effa8615065d89db5c8c063d74b7d3
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-05-05 22:22:04 +0300

    [Feature] Monitoring and Administration with Spring Boot Admin

--- catalogue-service/src/test/java/com/javaded/config/TestingBeans.java
+++ catalogue-service/src/test/java/com/javaded/config/TestingBeans.java
@@ -2,6 +2,7 @@ package com.javaded.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 import static org.mockito.Mockito.mock;
@@ -11,6 +12,12 @@ public class TestingBeans {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return mock(JwtDecoder.class);
+        return mock();
+    }
+
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return mock();
     }
 }
